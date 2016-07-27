2d4d7b4994964fb93e8911766e0df7d6688abf56
sam dunner <samdunner23@gmail.com>
2016-07-27 01:15:25 +0000

    Create class method multiple times

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,13 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+class Store < ActiveRecord::Base
+
+
+end
+
+burnaby = Store.create
+richmond = Store.create
+gastown = Store.create
+
+puts Store.count
