631aace6261c4446870f5b8690c099481d626ae1
sam dunner <samdunner23@gmail.com>
2016-07-27 02:00:17 +0000

    Add information to database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,13 +3,8 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-class Store < ActiveRecord::Base
-
-
-end
-
-burnaby = Store.create
-richmond = Store.create
-gastown = Store.create
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true,womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
